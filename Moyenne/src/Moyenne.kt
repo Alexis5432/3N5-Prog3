4c9ebf2f4a58c5d79c6c7604ab2131c8d9a64b3d
Alexis5432 <2277954@cegepmontpetit.ca>
2024-09-23 17:06:30 -0400

    exercise moyenne

--- Moyenne/src/Moyenne.kt
+++ Moyenne/src/Moyenne.kt
@@ -1,11 +1,31 @@
+
 import kotlin.random.Random
 
 fun main(){
-    creeTableauAleatoire(100)
+    println("Tableau: ${creeTableauAleatoire(5)} ")
+    somme(creeTableauAleatoire(5))
+    moyenne(creeTableauAleatoire(5))
+    min(creeTableauAleatoire(5))
+    max(creeTableauAleatoire(5))
 }
 
 fun creeTableauAleatoire(n: Int): Array<Int> {
     // ton code ici
-   return  Array(n) { Random.nextInt()}
+    return Array(n) {Random.nextInt(0,100)}
 }
-
+fun somme (tableau: Array<Int>){
+    val somme = tableau.sum()
+    println("La somme est de : $somme")
+}
+fun moyenne (tableau: Array<Int>){
+    val moyene = tableau.average()
+    println("La moyenne est de : $moyene")
+}
+fun min (tableau: Array<Int>){
+    val min = tableau.minOrNull()
+    println("Le minimum est de : $min ")
+}
+fun max (tableau: Array<Int>){
+    val max = tableau.max()
+    println("le maximum est de : $max")
+}
