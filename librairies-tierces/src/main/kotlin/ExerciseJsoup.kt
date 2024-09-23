260fe9575edba4a7205ee3ffd6ba214821ab9c4c
Alexis5432 <2277954@cegepmontpetit.ca>
2024-09-23 17:57:42 -0400

    Tierce

--- librairies-tierces/src/main/kotlin/ExerciseJsoup.kt
+++ librairies-tierces/src/main/kotlin/ExerciseJsoup.kt
@@ -8,5 +8,17 @@ fun main(args: Array<String>){
     var url = args[0]
     val doc: Document = Jsoup.connect(url).get()
     val balise: List<Element> = doc.select("a")
-    print(balise)
+    println(balise)
 }
+
+//String html = "<p>An <a href='http://example.com/'><b>example</b></a> link.</p>";
+//Document doc = Jsoup.parse(html);
+//Element link = doc.select("a").first();
+//
+//String text = doc.body().text(); // "An example link"
+//String linkHref = link.attr("href"); // "http://example.com/"
+//String linkText = link.text(); // "example""
+//
+//String linkOuterH = link.outerHtml();
+//// "<a href="http://example.com"><b>example</b></a>"
+//String linkInnerH = link.html(); // "<b>example</b>"
