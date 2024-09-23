3a969cc5f396d6f2b1dcffcb8787791a5b0d635e
Alexis5432 <2277954@cegepmontpetit.ca>
2024-09-23 17:39:17 -0400

    convertis

---
+++ Convertis/src/main/kotlin/convertis.kt
@@ -0,0 +1,15 @@
+package org.example
+
+fun main(){
+intTOfloat()
+}
+fun intTOfloat (){
+    val int :Int = 10
+    val float: Float = int.toFloat()
+    println("int to Float: $float")
+}
+fun doubleTOint(){
+    val double : Double = 10.00
+    val int: Int = double.toInt()
+    println("double to Int : $int")
+}
