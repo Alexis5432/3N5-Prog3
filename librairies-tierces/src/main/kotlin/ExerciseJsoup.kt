c729abb24c2669f6d49b81ebd4a3c1806fc592fa
Alexis5432 <2277954@cegepmontpetit.ca>
2024-09-24 17:28:27 -0400

    Fix exercise Jsoup

--- librairies-tierces/src/main/kotlin/ExerciseJsoup.kt
+++ librairies-tierces/src/main/kotlin/ExerciseJsoup.kt
@@ -7,8 +7,11 @@ import org.jsoup.nodes.Element
 fun main(args: Array<String>){
     var url = args[0]
     val doc: Document = Jsoup.connect(url).get()
-    val balise: List<Element> = doc.select("a")
-    println(balise)
+    val balise = doc.select("a")
+    for (i in balise.indices){
+        println("Liens: ${balise[i].attr("href")}")
+    }
+
 }
 
 //String html = "<p>An <a href='http://example.com/'><b>example</b></a> link.</p>";
