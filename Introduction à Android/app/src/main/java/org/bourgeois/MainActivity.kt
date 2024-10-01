b15fdd7662c58838a275c02cfbed325b68f9c872
Alexis Bourgeois <2277954@cegepmontpetit.ca>
2024-10-01 09:34:01 -0400

    FCT Exercise Italia

---
+++ Introduction à Android/app/src/main/java/org/bourgeois/MainActivity.kt
@@ -0,0 +1,23 @@
+package org.bourgeois
+
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import org.bourgeois.databinding.ActivityMainBinding
+
+private lateinit var binding: ActivityMainBinding
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        Log.i("Allo", "Mon supper message")
+
+    }
+}
